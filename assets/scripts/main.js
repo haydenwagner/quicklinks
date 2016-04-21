1c336b11fbf5d30458875cae172a38765617c3fa
Hayden Wagner <haydenblainewagner@gmail.com>
2016-04-20 22:32:39 -0400

    Working, need to delete old code and test further

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,32 +6,35 @@
     throw "Quicklinks needs jquery to work";
   }
 
-  var quicklinksObject, prototype, svgAssets;
+  var quicklinksObject, svgAssets, pageHeaders, topP;
+  var prototype = {};
 
-  svgAssets = [
+  prototype.svgAssets = [
     '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="59.951px" height="59.953px" viewBox="0 0 59.951 59.953" enable-background="new 0 0 59.951 59.953" xml:space="preserve"><g><g><path d="M14.341,37.396c9.767,0,19.533,0,29.3,0c3.225,0,3.225-5,0-5c-9.767,0-19.533,0-29.3,0C11.117,32.396,11.117,37.396,14.341,37.396L14.341,37.396z"/></g></g><g><g><path d="M28.892,46.396c4.917,0,9.833,0,14.749,0c3.225,0,3.225-5,0-5c-4.916,0-9.833,0-14.749,0C25.668,41.396,25.668,46.396,28.892,46.396L28.892,46.396z"/></g></g><g><g><path d="M23.091,28.396c6.85,0,13.7,0,20.55,0c3.225,0,3.225-5,0-5c-6.85,0-13.7,0-20.55,0C19.867,23.396,19.867,28.396,23.091,28.396L23.091,28.396z"/></g></g><g><g><path d="M14.341,19.396c9.767,0,19.533,0,29.3,0c3.225,0,3.225-5,0-5c-9.767,0-19.533,0-29.3,0C11.117,14.396,11.117,19.396,14.341,19.396L14.341,19.396z"/></g></g></svg>',
     '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="59.951px" height="59.953px" viewBox="0 0 59.951 59.953" enable-background="new 0 0 59.951 59.953" xml:space="preserve"><g><path d="M17.849,21.385c6.906,6.906,13.812,13.812,20.718,20.718c2.281,2.282,5.817-1.254,3.535-3.535c-6.906-6.906-13.812-13.813-20.718-20.718C19.103,15.568,15.567,19.104,17.849,21.385L17.849,21.385z"/></g><g><path d="M21.4,42.022c6.906-6.906,13.812-13.812,20.718-20.718c2.282-2.282-1.254-5.817-3.535-3.535c-6.906,6.906-13.812,13.812-20.718,20.718C15.583,40.769,19.118,44.304,21.4,42.022L21.4,42.022z"/></g></svg>'
   ];
 
 
 
-  function quicklinksInit(color){
+  window.quicklinksInit = function(color){
     //create a new prototype for our quicklinks object
-    //quicklinksCreatePrototype();
 
     //create a new quicklinks object with prototype defined below
     quicklinksObject = Object.create(prototype);
 
+    quicklinksCreatePanelLinks.call(quicklinksObject);
+
     //set the color used in this quicklinks instance as the value passed by the user
     quicklinksObject.color = color;
+    quicklinksObject.svg.style.fill = color;
 
     //append the quicklinks panel and quicklinks button to the DOM
     document.body.appendChild(quicklinksObject.panel);
     document.body.appendChild(quicklinksObject.button);
-  }
 
-
-
+    //add a listener to the window element that removes selected header and link styles
+    window.addEventListener('scroll', quicklinksObject.resetHeaderAndLinkStyles);
+  };
 
 
 
@@ -52,12 +55,10 @@
   //create the display button and add the correct svg
   prototype.button = document.createElement('div');
   prototype.button.className = 'quicklinks_mobile-button';
-  prototype.button.innerHTML = svgAssets[0];
+  prototype.button.innerHTML = prototype.svgAssets[0];
 
   //save a reference to the svg and set its color
   prototype.svg = $( prototype.button ).children('svg').toArray()[0];
-  prototype.svg.style.fill = prototype.color;
-
 
 
   //prevent touchmove events on the panel from scrolling the entire page
@@ -67,10 +68,10 @@
 
   //attach a listener on the button the opens or closes the panel
   prototype.button.addEventListener('click', function quicklinksButtonClickAnon(){
-    if ( quicklinks.panelVisible === false ) {
-      quicklinks.openPanel( this );
+    if ( quicklinksObject.panelVisible === false ) {
+      quicklinksObject.openPanel( this );
     } else {
-      quicklinks.closePanel( this );
+      quicklinksObject.closePanel( this );
     }
   });
 
@@ -82,18 +83,18 @@
    * @param {object} link - Clickable link created in quicklinks panel
    */
   prototype.topLinkClick = function(link){
-    quicklinks.inMotion = true;
+    this.inMotion = true;
 
     $('html, body').stop().animate({
       scrollTop: 0
     }, 500, function() {
-      quicklinks.inMotion = false;
+      this.inMotion = false;
     });
 
-    quicklinks.scrollLocation = 0;
+    this.scrollLocation = 0;
 
-    quicklinks.changeSectionStyle( link, "top");
-    quicklinks.changeLinkStyle( link );
+    this.changeSectionStyle( link, "top");
+    this.changeLinkStyle( link );
   };
 
 
@@ -105,6 +106,9 @@
    * @param {object/string} el - Element associated with link that page will scroll to when link is clicked. Special case "top" goes to scroll position 0
    */
   prototype.panelLinkClick = function(link, el){
+     console.log(link);
+     console.log(el);
+
     var windowHeight, location;
 
     windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
@@ -117,7 +121,7 @@
     $('html, body').stop().animate({
       scrollTop: location
     }, 500, function() {
-      quicklinks.inMotion = false;
+      this.inMotion = false;
     });
 
     this.scrollLocation = location;
@@ -136,7 +140,7 @@
     window.clearTimeout(this.timer);
 
     this.timer = window.setTimeout(function(){
-      quicklinks.closePanel(quicklinks.button);
+      this.closePanel(this.button);
     },1500);
   };
 
@@ -148,16 +152,18 @@
    * @param  {object} el - Button that was touched/clicked
    */
   prototype.openPanel = function(el){
-    $( quicklinks.panel ).stop().fadeIn(function makeLinksClickableAnon(){
+    $( this.panel ).stop().fadeIn(function makeLinksClickableAnon(){
       //after the panel is fully faded-in the links become clickable
-      quicklinks.panelLinksClickable = true;
+      quicklinksObject.panelLinksClickable = true;
     });
 
+    console.log(this);
+
     el.innerHTML = this.svgAssets[1];
-    el.style.background = quicklinks.color;
+    el.style.background = this.color;
     el.style.boxShadow = 'none';
 
-    quicklinks.panelVisible = true;
+    this.panelVisible = true;
   };
 
 
@@ -171,14 +177,14 @@
     //links are not clickable when the close process begins
     this.panelLinksClickable = false;
 
-    $( quicklinks.panel ).stop().fadeOut();
+    $( this.panel ).stop().fadeOut();
 
     el.innerHTML = this.svgAssets[0];
-    $( el ).children('svg')[0].style.fill = quicklinks.color;
+    $( el ).children('svg')[0].style.fill = this.color;
     el.style.background = 'white';
     el.style.boxShadow = '0px 3px 6px rgba(0,0,0,.5)';
 
-    quicklinks.panelVisible = false;
+    this.panelVisible = false;
   };
 
 
@@ -192,19 +198,19 @@
    */
   prototype.changeSectionStyle = function(link, el){
     if( el === "top"){
-      if( quicklinks.currEl ){
-        quicklinks.currEl.style.color = "#424242";
-        quicklinks.currEl = undefined;
+      if( this.currEl ){
+        this.currEl.style.color = "#424242";
+        this.currEl = undefined;
       }else{
-        quicklinks.currEl = undefined;
+        this.currEl = undefined;
       }
     }
     else{
-      if( quicklinks.currEl ){
-        quicklinks.currEl.style.color = "#424242";
-        quicklinks.currEl = el;
+      if( this.currEl ){
+        this.currEl.style.color = "#424242";
+        this.currEl = el;
       }else{
-        quicklinks.currEl = el;
+        this.currEl = el;
       }
     }
   };
@@ -217,17 +223,17 @@
    * @param  {object} el - The touched/clicked quicklinks link
    */
   prototype.changeLinkStyle = function(el){
-    if( quicklinks.currLink ){
-      $( quicklinks.currLink ).removeClass( "quicklinks--selected" );
-      quicklinks.currLink.style.borderColor = "#424242";
-      quicklinks.currLink.style.color = "#424242";
+    if( this.currLink ){
+      $( this.currLink ).removeClass( "quicklinks--selected" );
+      this.currLink.style.borderColor = "#424242";
+      this.currLink.style.color = "#424242";
     }
 
     el.className += " quicklinks--selected";
-    el.style.borderColor = quicklinks.color;
-    el.style.color = quicklinks.color;
+    el.style.borderColor = this.color;
+    el.style.color = this.color;
 
-    quicklinks.currLink = el;
+    this.currLink = el;
   };
 
 
@@ -236,26 +242,26 @@
    * Returns quicklink links and page header elements to default styling on scroll
    */
   prototype.resetHeaderAndLinkStyles = function() {
-    if( quicklinks.inMotion === true ) return;
+    if( this.inMotion === true ) return;
 
-    var topScrollBuffer = quicklinks.scrollLocation - 5;
-    var bottomScrollBuffer = quicklinks.scrollLocation + 5;
+    var topScrollBuffer = this.scrollLocation - 5;
+    var bottomScrollBuffer = this.scrollLocation + 5;
     var currentLocation =  $( window ).scrollTop();
 
     //cancel if within small buffer of +-5px from the elements scroll position
     if( currentLocation >= topScrollBuffer && currentLocation <= bottomScrollBuffer ) return;
 
-    if( quicklinks.currEl ){
-      quicklinks.currEl.style.color = "#424242";
-      quicklinks.currEl = undefined;
+    if( this.currEl ){
+      this.currEl.style.color = "#424242";
+      this.currEl = undefined;
     }
 
-    if( quicklinks.currLink ){
-      $( quicklinks.currLink ).removeClass( "quicklinks--selected" );
+    if( this.currLink ){
+      $( this.currLink ).removeClass( "quicklinks--selected" );
 
-      quicklinks.currLink.style.borderColor = "#424242";
-      quicklinks.currLink.style.color = "#424242";
-      quicklinks.currLink = undefined;
+      this.currLink.style.borderColor = "#424242";
+      this.currLink.style.color = "#424242";
+      this.currLink = undefined;
     }
   };
 
@@ -268,8 +274,7 @@
 
 
 
-  //add a listener to the window element that removes selected header and link styles
-  window.addEventListener('scroll', this.resetHeaderAndLinkStyles);
+
 
 
 
@@ -277,7 +282,7 @@
   /**
   * Gets all header elements on page and gives class and listeners, also makes 'top' page link
   */
-  (function quicklinksCreatePanelLinks(){
+  function quicklinksCreatePanelLinks(){
     var pageHeaders, topP;
 
     pageHeaders = $("h1, h2, h3, h4, h5, h6").toArray();
@@ -288,10 +293,13 @@
 
     topP.addEventListener('click', function topLinkClickAnon(){
       //checks if links are clickable before calling function -- links are not clickable when panel is closing
-      if( quicklinks.panelLinksClickable ) quicklinks.topLinkClick(topP);
+      console.log(this);
+      if (quicklinksObject.panelLinksClickable){
+        quicklinksObject.topLinkClick(topP);
+      }
     });
 
-    quicklinks.panel.appendChild(topP);
+    this.panel.appendChild(topP);
 
     for ( var i = 0; i < pageHeaders.length; i++ ) {
       var headerP = document.createElement("p");
@@ -300,14 +308,17 @@
 
       (function(h,j){
         headerP.addEventListener('click', function headerLinkClickAnon(){
+          console.log(this);
           //checks if links are clickable before calling function -- links are not clickable when panel is closing
-          if( quicklinks.panelLinksClickable ) quicklinks.panelLinkClick(h,pageHeaders[j]);
+          if (quicklinksObject.panelLinksClickable){
+            quicklinksObject.panelLinkClick(h,pageHeaders[j]);
+          }
         });
       })(headerP, i);
 
-      quicklinks.panel.appendChild(headerP);
+      this.panel.appendChild(headerP);
     }
-  })();
+  }
 
 
 
